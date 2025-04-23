5df12d8a027335ac7d8d1087ed99fb9bf40d5404
OthmaneBk <okebennani@gmail.com>
2025-04-23 22:05:11 +0100

    MockData

---
+++ data.py
@@ -0,0 +1,5 @@
+# Exemple de jeu de données de documents texte
+documents = [
+    "le medecin a prescrit des medicaments au patient pour traiter son infection",
+    "astronaute a observe la terre depuis la station spatiale en orbite autour de notre planete"
+]
